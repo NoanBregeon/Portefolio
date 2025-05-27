8d55590b917dba36b392cc044701eac21eeb5f52
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 12:14:25 +0200

    Create about.php

---
+++ Views/about.php
@@ -0,0 +1,4 @@
+<section class="about">
+  <h1>À propos de moi</h1>
+  <p>Bienvenue sur mon portfolio ! Je suis un développeur passionné par la création de sites web et d'applications. Actuellement en BTS SIO SLAM, je cherche à améliorer mes compétences et à participer à des projets enrichissants.</p>
+</section>
