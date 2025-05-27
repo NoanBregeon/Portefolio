896c03c1ba49b993dac2a3473c26e32f7c6954a1
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 13:25:35 +0200

    Create telechargements.php

---
+++ Views/telechargements.php
@@ -0,0 +1,7 @@
+<section class="telechargements">
+  <h1>Téléchargements</h1>
+  <ul>
+    <li><a href="assets/files/CV_Noan_Bregeon.pdf" download>Télécharger mon CV</a></li>
+    <li><a href="assets/files/Portfolio_Noan_Bregeon.pdf" download>Télécharger mon portfolio</a></li>
+  </ul>
+</section>
