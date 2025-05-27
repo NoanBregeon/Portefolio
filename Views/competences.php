32f122dc54fcaee12e694dfcd87089363a53d988
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 13:25:26 +0200

    Create competences.php

---
+++ Views/competences.php
@@ -0,0 +1,9 @@
+<section class="competences">
+  <h1>Mes Compétences</h1>
+  <ul>
+    <li><strong>Langages :</strong> PHP, JavaScript, Python, HTML, CSS</li>
+    <li><strong>Frameworks :</strong> Laravel, React, Bootstrap</li>
+    <li><strong>Outils :</strong> Git, Docker, MySQL</li>
+    <li><strong>Certifications :</strong> OpenClassrooms - Développeur Web</li>
+  </ul>
+</section>
