10a8617ab83c2526bc44d500d642d0df2aacbf0a
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 12:14:07 +0200

    Delete contact.php

--- contact.php
+++
@@ -1,9 +0,0 @@
-<section class="contact">
-  <h1>Contactez-moi</h1>
-  <p>Vous pouvez me joindre via les moyens suivants :</p>
-  <ul>
-    <li>Email : <a href="mailto:monemail@example.com">monemail@example.com</a></li>
-    <li>Téléphone : +33 6 12 34 56 78</li>
-    <li>LinkedIn : <a href="https://linkedin.com/in/monprofil" target="_blank">Mon Profil LinkedIn</a></li>
-  </ul>
-</section>
