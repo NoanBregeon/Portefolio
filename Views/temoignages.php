dfff9e1827f2ee26147b05126ca73e0d1a55f509
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 13:25:38 +0200

    Create temoignages.php

---
+++ Views/temoignages.php
@@ -0,0 +1,11 @@
+<section class="temoignages">
+  <h1>Témoignages</h1>
+  <blockquote>
+    <p>"Noan est un développeur talentueux et très professionnel. Il a su répondre à nos besoins avec efficacité."</p>
+    <cite>- Client A</cite>
+  </blockquote>
+  <blockquote>
+    <p>"Travailler avec Noan a été un plaisir. Il est très compétent et toujours à l'écoute."</p>
+    <cite>- Collaborateur B</cite>
+  </blockquote>
+</section>
