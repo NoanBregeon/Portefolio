4574f88e36cf34026a2d1219b8075ef2e22ca3b2
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 11:37:08 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Exemple de script simple : scroll vers le haut en cliquant sur le pied de page
-document.querySelector('footer').addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-});
