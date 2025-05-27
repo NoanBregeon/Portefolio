bfaed336ba994f70facc7d4527598a3f5523c681
Noan_Bregeon <furienocturne2.0@gmail.com>
2025-05-27 13:25:32 +0200

    Create statistiques.php

---
+++ Views/statistiques.php
@@ -0,0 +1,8 @@
+<section class="statistiques">
+  <h1>Statistiques</h1>
+  <ul>
+    <li><strong>Projets réalisés :</strong> 10</li>
+    <li><strong>Lignes de code écrites :</strong> 50 000+</li>
+    <li><strong>Contributions GitHub :</strong> 200+ cette année</li>
+  </ul>
+</section>
